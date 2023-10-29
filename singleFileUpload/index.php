0f67adf995e3d6594707aeab04f6b28ea1584601
diaabashtawi <diaabashtawi@gmail.com>
2023-10-29 14:12:37 +0400

    adding singleFileUpload directory

---
+++ singleFileUpload/index.php
@@ -0,0 +1,18 @@
+<?php
+
+
+
+
+
+
+
+
+
+?>
+
+
+
+<form method="post" action="" class="" enctype="multipart/form-data">
+    <input type="file" name="myFile" value="">
+    <input type="submit" value="Upload">
+</form>
