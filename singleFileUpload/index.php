0f6f3ff78df2d91ddb38a4bfede1f3e8d181fa95
diaabashtawi <diaabashtawi@gmail.com>
2023-10-29 17:41:05 +0400

    adding allowed extention array for file

--- singleFileUpload/index.php
+++ singleFileUpload/index.php
@@ -4,6 +4,14 @@
 
 
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+
+    // Setting Errors Array 
+    $errors = array();
+
+    // Set Allowed File Extentions
+    $allwoed_file_extentions = array('jpg', 'gif', 'jpeg', 'png');
+
     $myFile = $_FILES['myFile'];
 
     echo '<pre>';
@@ -11,7 +19,6 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
     print_r($myFile);
 
     echo '</pre>';
-
 
     $file_name = $_FILES['myFile']['name'];
     $file_type = $_FILES['myFile']['type'];
@@ -24,8 +31,30 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
     echo 'File size : ' . $file_size. '<br>';
     echo 'File temp name : ' . $file_tmp. '<br>';
 
+    // Get file extention
 
-    move_uploaded_file($file_tmp, $_SERVER['DOCUMENT_ROOT'].'/phpUploadFiles/singleFileUpload/upload/'.$file_name );
+    $file_extention = explode('.', $file_name);
+    $file_extention = strtolower(end($file_extention));
+    
+    echo $file_extention.'<br>';
+
+    // Checking the File size
+    if($file_size > 8000000){
+        $errors[] = '<div>File size cannot be more than x </div>';
+    }
+
+
+    // If there is no ERRORS uploade the file 
+    if(empty($errors)){
+        move_uploaded_file($file_tmp, $_SERVER['DOCUMENT_ROOT'].'/phpUploadFiles/singleFileUpload/upload/'.$file_name );
+        echo 'File Uploaded Successfully';
+    }else{
+        foreach($errors as $error){
+            echo $error;
+        }
+    }
+
+    
 
 }
 
