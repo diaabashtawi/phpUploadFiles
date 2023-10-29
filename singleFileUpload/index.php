d132952f8c8e4af7a827e82d46240479c3a8fb5c
diaabashtawi <diaabashtawi@gmail.com>
2023-10-29 14:34:24 +0400

    adding the php upload script

--- singleFileUpload/index.php
+++ singleFileUpload/index.php
@@ -3,6 +3,28 @@
 
 
 
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    $myFile = $_FILES['myFile'];
+
+    echo '<pre>';
+
+    print_r($myFile);
+
+    echo '</pre>';
+
+
+    $file_type = $_FILES['myFile']['type'];
+    $file_size = $_FILES['myFile']['size'];
+    $file_tmp = $_FILES['myFile']['tmp_name'];
+
+    echo 'File type : ' . $file_type. '<br>';
+    echo 'File size : ' . $file_size. '<br>';
+    echo 'File temp name : ' . $file_tmp. '<br>';
+
+
+    move_uploaded_file($file_tmp,);
+}
+
 
 
 
