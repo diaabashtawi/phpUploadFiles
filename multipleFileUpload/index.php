4932a46a3f9fff1a473677ba6319e4eacef0199f
diaabashtawi <diaabashtawi@gmail.com>
2023-11-04 14:31:39 +0400

    adding errors array, allowed file extenions array and the myFiles vairable

--- multipleFileUpload/index.php
+++ multipleFileUpload/index.php
@@ -1,7 +1,26 @@
 <?php 
 
 
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    // Setting Errors Array
+    $errors = array();
 
+    // Setting allowed file extentions
+    $allowed_file_extentions = array(
+        'jpg',
+        'gif',
+        'jpeg',
+        'png'
+    );
+
+    $myFiles = $_FILES['myFiles'];
+    echo '<pre>';
+    print_r($myFiles);
+    echo '<pre>';
+
+
+
+}
 
 
 
@@ -10,6 +29,6 @@
 
 
 <form class="" action="" method="post" enctype="multipart/form-data">
-<input type="file" name="myFile[]" value="" multiple="multiple">
+<input type="file" name="myFiles[]" value="" multiple="multiple">
     <input type="submit" value="Upload">
 </form>
