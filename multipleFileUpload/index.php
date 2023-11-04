075143dce3ab321cb7914e75650f265ab44f544d
diaabashtawi <diaabashtawi@gmail.com>
2023-11-04 14:23:40 +0400

    adding multipleFileUpload directory

---
+++ multipleFileUpload/index.php
@@ -0,0 +1,15 @@
+<?php 
+
+
+
+
+
+
+?>
+
+
+
+<form class="" action="" method="post" enctype="multipart/form-data">
+<input type="file" name="myFile[]" value="" multiple="multiple">
+    <input type="submit" value="Upload">
+</form>
