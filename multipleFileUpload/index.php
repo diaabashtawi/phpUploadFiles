eb25e017d38d3cec9dc3f479c11b1bb73d2274ce
diaabashtawi <diaabashtawi@gmail.com>
2023-12-01 23:43:09 +0400

    checking on the Files extentions and if the user upload file or not

--- multipleFileUpload/index.php
+++ multipleFileUpload/index.php
@@ -16,11 +16,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         'png'
     );
 
-    
-    // echo '<pre>';
-    // print_r($myFiles);
-    // echo '<pre>';
-
     // Get info from the html form
     $files_name = $myFiles['name'];
     $files_full_path = $myFiles['full_path'];
@@ -29,51 +24,48 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $files_size = $myFiles['size'];
     $files_error = $myFiles['error'];
 
+    if ($files_error[0] == 4) {// if the error value is 4 then no file uploaded
+        echo '<div>No File Uploaded</div>';
+    } else {
+        // This line to count how many files has been uploaded in the array
+        $files_count = count($files_name);
 
-    // echo "Files Name : " . $files_name. "<br>";
-    // echo "Files full path : " . $files_full_path. "<br>";
-    // echo "Files type : " . $files_type. "<br>";
-    // echo "Files temp name : " . $files_temp_name. "<br>";
-    // echo "Files size : " . $files_size. "<br>";
-    // echo "Files error : " . $files_error. "<br>";
+        for ($i = 0; $i < $files_count; $i++) {
+            // Setting Errors Array
+            $errors = array();
 
-    // This line to count how many files has been uploaded in the array
-    $files_count = count($files_name);
-    // echo $files_count;
-    // echo '<br>';
-    for ($i = 0; $i < $files_count; $i++) {
-        // Setting Errors Array
-        $errors = array();
-
-        if($files_size[$i] > 100000){
+            //Get Files Extention
+            $files_extention = explode('.' , $files_name[$i]);
+            $files_extention = strtolower(end($files_extention));
             
-            $errors[] = '<div> Too Large File  </div>';
-        }
 
-        if(empty($errors)){
-            // Move all the files if there is no errors 
-            move_uploaded_file($files_temp_name[$i], $_SERVER['DOCUMENT_ROOT'] . '/phpUploadFiles/multipleFileUpload/upload/' . $files_name[$i]);
-            echo '<div style="background-color: #eee; padding: 10px; margin-bottom: 20px;">';
-            echo 'File number ' . ($i+1) . ' Uploaded Successfully ' . ' file name : '. $files_name[$i] .'<br>';
-            echo '</div>';
-        }else{
-            echo '<div style="background-color: #eee; padding: 10px; margin-bottom: 20px;">';
-            echo 'Errors for file number ' . ($i+1) . ' file name : '. $files_name[$i] .'<br>';
-            foreach ($errors as $error){
-                echo $error;
+            if ($files_size[$i] > 100000) {
+                $errors[] = '<div> Too Large File  </div>';
             }
-            echo '</div>';
+
+            if(!in_array($files_extention, $allowed_file_extentions)){
+                $errors[] = '<div> File extentions NOT VALID </div>';
+            }
+            if (empty($errors)) {
+                // Move all the files if there is no errors 
+                move_uploaded_file($files_temp_name[$i], $_SERVER['DOCUMENT_ROOT'] . '/phpUploadFiles/multipleFileUpload/upload/' . $files_name[$i]);
+                echo '<div style="background-color: #eee; padding: 10px; margin-bottom: 20px;">';
+                echo 'File number ' . ($i + 1) . ' Uploaded Successfully ' . ' file name : ' . $files_name[$i] . '<br>';
+                echo '</div>';
+            } else {
+                echo '<div style="background-color: #eee; padding: 10px; margin-bottom: 20px;">';
+                echo 'Errors for file number ' . ($i + 1) . ' file name : ' . $files_name[$i] . '<br>';
+                foreach ($errors as $error) {
+                    echo $error;
+                }
+                echo '</div>';
+            }
         }
     }
 }
-
-
-
 ?>
 
-
-
 <form class="" action="" method="post" enctype="multipart/form-data">
-    <input type="file" name="myFiles[]" value="" multiple="multiple">
+    <input type="file" name="myFiles[]" value="" multiple="multiple" requiredn>
     <input type="submit" value="Upload">
 </form>
